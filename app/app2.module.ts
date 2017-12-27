7574280f60bf538ae1341bf21a5887a658f0043a
abhishek-ghode <abhishek.ghode@capgemini.com>
2017-12-27 16:15:34 +0530

    Created a service now

--- app/app2.module.ts
+++ app/app2.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } 
 from "@angular/platform-browser";
 import { AppComponent } from "./app2.component";
+import { ProductService } from "./products/products.service";
 
 @NgModule({
         imports:[BrowserModule],
         declarations:[AppComponent],
-        bootstrap:[AppComponent]
+        bootstrap:[AppComponent],
+        providers: [ProductService]
     }
 )
 export class AppModule{
